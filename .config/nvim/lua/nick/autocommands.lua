1c2255d0edb10a37083b6d52aa66d0eee9e513b7
Nick Conway <nick@conway.dev>
2022-02-23 21:04:33 -0500

    Update config

--- .config/nvim/lua/nick/autocommands.lua
+++ .config/nvim/lua/nick/autocommands.lua
@@ -8,13 +8,14 @@ vim.cmd([[
 
 vim.cmd([[
     augroup MAIN
-    autocmd!
-    autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout = 40})
-    autocmd BufWritePre * :call TrimWhitespace()
-    autocmd InsertLeave * set iminsert=0
-    autocmd FileType which_key set laststatus=0 noshowmode noruler
-    autocmd BufLeave <buffer> set laststatus=2 showmode ruler
-    autocmd VimResized * wincmd =
-    autocmd TextChanged,InsertLeave * if &readonly == 0 && filereadable(bufname('%')) | silent write | endif " autosave
+        autocmd!
+        autocmd BufWritePost plugins.lua source <afile> | PackerSync
+        autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout = 40})
+        autocmd BufWritePre * :call TrimWhitespace()
+        autocmd InsertLeave * set iminsert=0
+        autocmd FileType which_key set laststatus=0 noshowmode noruler
+        autocmd BufLeave <buffer> set laststatus=2 showmode ruler
+        autocmd VimResized * wincmd =
+        autocmd TextChanged,InsertLeave * if &readonly == 0 && filereadable(bufname('%')) | silent write | endif " autosave
     augroup END
 ]])
--- .config/nvim/lua/nick/plugins.lua
+++ .config/nvim/lua/nick/plugins.lua
@@ -1,57 +1,56 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
 if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system({ 'git', 'clone', '--depth', 1, 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
-vim.cmd([[
-    augroup packer
-        autocmd!
-        autocmd BufWritePost plugins.lua source <afile> | PackerSync
-    augroup end
-]])
+function get_config(name)
+    return string.format("require('plugins/%s')", name)
+end
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
+
     use 'onsails/lspkind-nvim'
     use 'neovim/nvim-lspconfig'
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
+    use { 'hrsh7th/nvim-cmp', config = get_config('cmp') }
 
-    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
     use 'nvim-lua/popup.nvim'
     use 'nvim-lua/plenary.nvim'
-    use 'nvim-telescope/telescope.nvim'
+    use { 'nvim-telescope/telescope.nvim', config = get_config('telescope') }
     use 'nvim-telescope/telescope-fzy-native.nvim'
 
-    use 'folke/which-key.nvim'
+    use { 'folke/which-key.nvim', config = get_config('which-key') }
 
     use 'SirVer/ultisnips'
     use 'quangnguyen30192/cmp-nvim-ultisnips'
 
     use 'tpope/vim-obsession'
     use 'dhruvasagar/vim-prosession'
-    use 'numToStr/Comment.nvim'
+    use { 'numToStr/Comment.nvim', config = get_config('Comment') }
 
     use 'kyazdani42/nvim-web-devicons'
-    use 'lewis6991/gitsigns.nvim'
+    use { 'lewis6991/gitsigns.nvim', config = get_config('gitsigns') }
     use 'kdheepak/tabline.nvim'
 
-    use 'nvim-lualine/lualine.nvim'
+    use { 'nvim-lualine/lualine.nvim', config = get_config('lualine') }
 
-    use 'navarasu/onedark.nvim'
+    use { 'navarasu/onedark.nvim', config = get_config('onedark') }
 
-    use 'windwp/nvim-autopairs'
+    use { 'windwp/nvim-autopairs', config = get_config('autopairs') }
 
     use 'nathom/tmux.nvim'
 
     use 'sbdchd/neoformat'
 
-    use 'akinsho/toggleterm.nvim'
+    use { 'akinsho/toggleterm.nvim', config = get_config('toggleterm') }
 
     if packer_bootstrap then
         require('packer').sync()
