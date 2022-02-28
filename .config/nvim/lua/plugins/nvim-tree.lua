3aa5a554533b44197bf6a707058e9c598da14b05
Nick Conway <nick@conway.dev>
2022-02-28 11:40:50 -0500

    Update config

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,1 +1,10 @@
-require('nvim-tree').setup{}
+-- lua
+vim.g.nvim_tree_respect_buf_cwd = 1
+
+require("nvim-tree").setup({
+  update_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true
+  },
+})
