ba46fa4f1299304cc9af1aa1de586162ed918bbd
Nick Conway <nick@conway.dev>
2022-11-02 19:26:08 -0400

    Update config

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -80,7 +80,7 @@ cmp.setup.cmdline(':', {
     }
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require('lspconfig').vimls.setup{
     capabilities = capabilities
