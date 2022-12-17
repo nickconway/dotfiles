47601302b777cd7ca60a7007c6ba04d1519b509d
Nick Conway <nick@conway.dev>
2022-12-17 02:10:11 -0500

    Update config

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,18 +1,28 @@
+local c = require("onedark.colors")
+local transparent = require("lualine.themes.onedark")
+
+transparent.normal.a.bg = "none"
+transparent.normal.a.fg = c.grey
+transparent.normal.b.bg = "none"
+transparent.normal.b.fg = c.grey
+transparent.normal.c.bg = "none"
+transparent.normal.c.fg = c.grey
+
 require('lualine').setup{
     options = {
-        theme = 'transparent_onedark',
+        theme = transparent,
         section_separators = {left = '', right = ''},
         component_separators = {left = '', right = ''},
         padding = 1,
         globalstatus=true,
     },
     sections = {
-        lualine_x = {'tabs', 'encoding', 'fileformat', 'filetype'}
+        lualine_x = {'encoding', 'fileformat', 'filetype'}
     },
     inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {'filename'},
+        lualine_c = {},
         lualine_x = {},
         lualine_y = {},
         lualine_z = {},
