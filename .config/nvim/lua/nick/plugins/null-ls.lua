ad1d8e4b52e780668c3a678cfc47a65a4d6daaf3
Nick Conway <nick@conway.dev>
2022-12-26 22:26:35 -0500

    Update config

--- .config/nvim/lua/nick/plugins/null-ls.lua
+++ .config/nvim/lua/nick/plugins/null-ls.lua
@@ -3,7 +3,9 @@ return {
     config = function()
         require("null-ls").setup({
             sources = {
-                require("null-ls").builtins.formatting.prettier_eslint
+                require("null-ls").builtins.diagnostics.code_actions,
+                require("null-ls").builtins.formatting.prettier,
+                require("null-ls").builtins.diagnostics.eslint,
             }
         })
     end
