fc1ce0ada8a2a0c322eb30d3c4a727f446365b39
Nick Conway <nick@conway.dev>
2023-06-01 21:23:06 -0400

    update config

--- .config/nvim/lua/nick/plugins/lualine.lua
+++ .config/nvim/lua/nick/plugins/lualine.lua
@@ -20,15 +20,20 @@ return {
                 globalstatus = true,
             },
             sections = {
-                lualine_x = { 'encoding', 'fileformat', 'filetype' }
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
             },
             inactive_sections = {
                 lualine_a = {},
                 lualine_b = {},
-                lualine_c = {},
-                lualine_x = {},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
                 lualine_y = {},
-                lualine_z = {},
+                lualine_z = {}
             },
         }
     end
