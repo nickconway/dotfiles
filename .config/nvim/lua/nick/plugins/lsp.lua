5bf50996aca2a1c35191bac09a5a3d44a9aadcea
Nick Conway <nick@conway.dev>
2023-06-08 00:20:50 -0400

    Update config

--- .config/nvim/lua/nick/plugins/lsp.lua
+++ .config/nvim/lua/nick/plugins/lsp.lua
@@ -1,55 +1,63 @@
 return {
-	"VonHeikemen/lsp-zero.nvim",
-	branch = "v2.x",
-	dependencies = {
-		-- LSP Support
-		{ "neovim/nvim-lspconfig" },
-		{
-			"williamboman/mason.nvim",
-			build = function()
-				pcall(vim.cmd, "MasonUpdate")
-			end,
-		},
-		{ "williamboman/mason-lspconfig.nvim" },
-		{ "simrat39/rust-tools.nvim" },
-		{ "folke/neodev.nvim" },
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v2.x",
+    dependencies = {
+        -- LSP Support
+        { "neovim/nvim-lspconfig" },
+        {
+            "williamboman/mason.nvim",
+            build = function()
+                pcall(vim.cmd, "MasonUpdate")
+            end,
+        },
+        { "williamboman/mason-lspconfig.nvim" },
+        { "simrat39/rust-tools.nvim" },
+        { "folke/neodev.nvim" },
 
-		-- Autocompletion
-		{ "hrsh7th/nvim-cmp" },
-		{ "hrsh7th/cmp-buffer" },
-		{ "hrsh7th/cmp-path" },
-		{ "saadparwaiz1/cmp_luasnip" },
-		{ "hrsh7th/cmp-nvim-lsp" },
-		{ "hrsh7th/cmp-nvim-lua" },
+        -- Autocompletion
+        { "hrsh7th/nvim-cmp" },
+        { "hrsh7th/cmp-buffer" },
+        { "hrsh7th/cmp-path" },
+        { "saadparwaiz1/cmp_luasnip" },
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-nvim-lua" },
 
-		-- Snippets
-		{ "L3MON4D3/LuaSnip" },
-		{ "rafamadriz/friendly-snippets" },
+        -- Snippets
+        { "L3MON4D3/LuaSnip" },
+        { "rafamadriz/friendly-snippets" },
 
-		{ "onsails/lspkind-nvim" },
-	},
-	config = function()
-		require("neodev").setup({
-			library = { plugins = { "nvim-dap-ui" }, types = true },
-		})
+        { "onsails/lspkind-nvim" },
+    },
+    config = function()
+        require("neodev").setup({
+            library = { plugins = { "nvim-dap-ui" }, types = true },
+        })
 
-		local lsp = require("lsp-zero").preset("recommended")
+        local lsp = require("lsp-zero").preset("recommended")
 
-		lsp.ensure_installed({
-			"tsserver",
-			"eslint",
-			"lua_ls",
-			"rust_analyzer",
-		})
+        lsp.ensure_installed({
+            "tsserver",
+            "eslint",
+            "lua_ls",
+            "rust_analyzer",
+        })
 
-		lsp.on_attach(function(client, bufnr)
-			if client.server_capabilities.documentSymbolProvider then
-				require("nvim-navic").attach(client, bufnr)
-			end
-			lsp.default_keymaps({ buffer = bufnr })
-		end)
+        lsp.on_attach(function(client, bufnr)
+            if client.server_capabilities.documentSymbolProvider then
+                require("nvim-navic").attach(client, bufnr)
+            end
+            lsp.default_keymaps({ buffer = bufnr })
+        end)
 
-		lsp.nvim_workspace()
-		lsp.setup()
-	end,
+        lsp.nvim_workspace()
+        lsp.setup()
+
+        local cmp = require('cmp')
+
+        cmp.setup({
+            mapping = {
+                ['<CR>'] = cmp.mapping.confirm({ select = false }),
+            }
+        })
+    end,
 }
