7dcbad0b0522f6ba3ea35ce0df8f6c9abba0a892
Nick Conway <nick@conway.dev>
2023-06-08 19:54:24 -0400

    Update config

--- .config/nvim/lua/nick/plugins/noice.lua
+++ .config/nvim/lua/nick/plugins/noice.lua
@@ -8,36 +8,35 @@ return {
         --   If not available, we `mini` as the fallback,
         "rcarriga/nvim-notify",
     },
+    opts = {
+        routes = {
+            {
+                view = nil,
+                filter = { find = "Format request failed" },
+            }
+        },
+        lsp = {
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
+            },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+            bottom_search = false,            -- use a classic bottom cmdline for search
+            command_palette = true,           -- position the cmdline and  together
+            long_message_to_split = true,     -- long messages will be sent to a split
+            inc_rename = false,               -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = false,           -- add a border to hover docs and signature help
+        },
+    },
     config = function()
         vim.opt.termguicolors = true
 
         require("notify").setup({
             background_colour = "#000000",
-        })
-
-        require("noice").setup({
-            routes = {
-                {
-                    view = nil,
-                    filter = { find = "Format request failed" },
-                }
-            },
-            lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
-                },
-            },
-            -- you can enable a preset for easier configuration
-            presets = {
-                bottom_search = false,        -- use a classic bottom cmdline for search
-                command_palette = true,       -- position the cmdline and  together
-                long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false,       -- add a border to hover docs and signature help
-            },
         })
     end
 }
