b26faa4d988fc7bd6fbdd59ddb3d0b58c3214a63
Nick Conway <nick@conway.dev>
2023-06-08 00:42:17 -0400

    Update config

--- .config/nvim/lua/nick/plugins/lsp.lua
+++ .config/nvim/lua/nick/plugins/lsp.lua
@@ -75,23 +75,13 @@ return {
                 ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
             },
             formatting = {
-                -- changing the order of fields so the icon is the first
-                fields = { 'menu', 'abbr', 'kind' },
-
-                -- here is where the change happens
-                format = function(entry, item)
-                    local menu_icon = {
-                        nvim_lsp = 'λ',
-                        luasnip = '⋗',
-                        buffer = 'Ω',
-                        path = '🖫',
-                        nvim_lua = 'Π',
-                    }
-
-                    item.menu = menu_icon[entry.source.name]
-                    return item
-                end,
-            },
+                fields = { 'abbr', 'kind', 'menu' },
+                format = require('lspkind').cmp_format({
+                    mode = 'symbol',       -- show only symbol annotations
+                    maxwidth = 50,         -- prevent the popup from showing more than provided characters
+                    ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+                })
+            }
         })
     end,
 }
