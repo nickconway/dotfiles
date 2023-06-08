eccc99150f0a2ad050b48d0caf4cfb01b58a208d
Nick Conway <nick@conway.dev>
2023-06-08 00:36:41 -0400

    Update config

--- .config/nvim/lua/nick/plugins/lsp.lua
+++ .config/nvim/lua/nick/plugins/lsp.lua
@@ -49,15 +49,49 @@ return {
             lsp.default_keymaps({ buffer = bufnr })
         end)
 
+        lsp.set_sign_icons({
+            error = '✘',
+            warn = '▲',
+            hint = '⚑',
+            info = '»'
+        })
+
+        lsp.set_server_config({
+            on_init = function(client)
+                client.server_capabilities.semanticTokensProvider = nil
+            end,
+        })
+
         lsp.nvim_workspace()
         lsp.setup()
 
         local cmp = require('cmp')
+        local cmp_action = require('lsp-zero').cmp_action()
 
         cmp.setup({
             mapping = {
                 ['<CR>'] = cmp.mapping.confirm({ select = false }),
-            }
+                ['<Tab>'] = cmp_action.luasnip_supertab(),
+                ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+            },
+            formatting = {
+                -- changing the order of fields so the icon is the first
+                fields = { 'menu', 'abbr', 'kind' },
+
+                -- here is where the change happens
+                format = function(entry, item)
+                    local menu_icon = {
+                        nvim_lsp = 'λ',
+                        luasnip = '⋗',
+                        buffer = 'Ω',
+                        path = '🖫',
+                        nvim_lua = 'Π',
+                    }
+
+                    item.menu = menu_icon[entry.source.name]
+                    return item
+                end,
+            },
         })
     end,
 }
