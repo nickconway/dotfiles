c3231c7af3174836b733441169975a842355e2f9
Nick Conway <nick@conway.dev>
2023-07-25 00:46:29 -0400

    Update config

--- .config/nvim/lua/nick/plugins/mini.lua
+++ .config/nvim/lua/nick/plugins/mini.lua
@@ -1,6 +1,6 @@
 return {
     "echasnovski/mini.nvim",
-    version = "false",
+    version = false,
     config = function()
         require("mini.ai").setup()
         require("mini.jump").setup()
