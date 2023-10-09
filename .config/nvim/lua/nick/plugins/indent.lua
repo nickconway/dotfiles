187b1e7e2237815a323c0fd2957be4b7a9fbfd34
Nick Conway <nick@conway.dev>
2023-10-09 00:08:26 -0400

    Update config

--- .config/nvim/lua/nick/plugins/indent.lua
+++ .config/nvim/lua/nick/plugins/indent.lua
@@ -1,12 +1,5 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
-    config = function()
-        require("indent_blankline").setup {
-            space_char_blankline = " ",
-            show_current_context = true,
-            show_current_context_start = true,
-            use_treesitter = true,
-            use_tresitter_scope = true,
-        }
-    end
+    main = "ibl",
+    opts = {},
 }
