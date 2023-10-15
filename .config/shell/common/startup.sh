5f64e3cc6696248994b00c98eac574421cd39090
Nick Conway <nick@conway.dev>
2023-10-14 22:13:58 -0400

    Update config

--- .config/shell/common/startup.sh
+++ .config/shell/common/startup.sh
@@ -1,5 +1,4 @@
 command -v devbox > /dev/null && eval "$(devbox global shellenv --init-hook)"
-command -v keychain > /dev/null && eval `keychain -q --nogui --eval $(find $HOME/.ssh/ -name "id*" -not -path "*.pub")`
 command -v starship > /dev/null && eval "$(starship init $SHELL_NAME)"
 command -v zoxide > /dev/null && eval "$(zoxide init $SHELL_NAME --cmd cd)"
 
@@ -10,14 +9,15 @@ fi
 
 if [[ -z $TMUX ]] && [[ -z $ZELLIJ ]]; then
     if [[ -z $SSH_CONNECTION ]]; then
-        tmux new -As main
+        tmux -vv new -As main
         # zellij a -c main
     elif [[ $SSH_HOSTNAME != "" ]] && [[ -n SSH_HOSTNAME ]]; then
-        tmux new -As ssh-$SSH_HOSTNAME; exit
+        tmux -vv new -As ssh-$SSH_HOSTNAME; exit
         # zellij a -c ssh-$SSH_HOSTNAME; exit
     else
         # zellij a -c ssh; exit
-        tmux new -As ssh; exit
+        tmux -vv new -As ssh; exit
     fi
 fi
 
+command -v keychain > /dev/null && eval `keychain -q --nogui --eval $(find $HOME/.ssh/ -name "id*" -not -path "*.pub")`
