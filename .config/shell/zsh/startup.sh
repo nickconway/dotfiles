895038f8afaf10df7f011f41e133d7c72b95aef3
Nick Conway <nick@conway.dev>
2023-10-16 19:14:26 -0400

    Update config

--- .config/shell/zsh/startup.sh
+++ .config/shell/zsh/startup.sh
@@ -1,2 +1,4 @@
 [[ -e "$DEVBOX_PROFILE/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]] && source "$DEVBOX_PROFILE/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 [[ -e "$DEVBOX_PROFILE/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]] && source "$DEVBOX_PROFILE/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+
+set -o vi
