4f87256cf9fbbe02a84207fe89d8cf982eba350e
Nick Conway <nick@conway.dev>
2023-10-24 10:18:38 -0400

    Update config

--- .config/shell/common/startup.sh
+++ .config/shell/common/startup.sh
@@ -1,13 +1,13 @@
 if [[ -z $TMUX ]] && [[ -z $ZELLIJ ]]; then
     if [[ -z $SSH_CONNECTION ]]; then
-        tmux new -As main
+        command -v tmux > /dev/null && tmux new -As main
         # zellij a -c main
     # elif [[ $SSH_HOSTNAME != "" ]] && [[ -n SSH_HOSTNAME ]]; then
     #     tmux new -As ssh-$SSH_HOSTNAME; exit
     #     # zellij a -c ssh-$SSH_HOSTNAME; exit
     else
         # zellij a -c ssh; exit
-        tmux new -As ssh; exit
+        command -v tmux > /dev/null && tmux new -As ssh; exit
     fi
 fi
 
