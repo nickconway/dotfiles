d61dd364c1053b8f6156e3a201e6cdf64e01710a
Nick Conway <nick@conway.dev>
2023-10-25 23:07:57 -0400

    Update config

--- .config/shell/common/startup.sh
+++ .config/shell/common/startup.sh
@@ -1,4 +1,4 @@
-command -v termux-reload-settings > /dev/null && ssh-picker
+command -v termux-reload-settings > /dev/null && ssh-picker && return
 
 if [[ -z $TMUX ]] && [[ -z $ZELLIJ ]]; then
     if [[ -z $SSH_CONNECTION ]]; then
