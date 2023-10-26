c30e16b86fb81dfb4668f08013ab049e7e9e748f
Nick Conway <nick@conway.dev>
2023-10-25 22:51:00 -0400

    Update config

--- .config/shell/common/startup.sh
+++ .config/shell/common/startup.sh
@@ -1,3 +1,5 @@
+command -v termux-reload-settings > /dev/null && ssh-picker && exit
+
 if [[ -z $TMUX ]] && [[ -z $ZELLIJ ]]; then
     if [[ -z $SSH_CONNECTION ]]; then
         command -v tmux > /dev/null && tmux new -As main
@@ -17,7 +19,5 @@ if [ -n "${commands[fzf-share]}" ]; then
     source "$(fzf-share)/key-bindings.$SHELL_NAME"
     source "$(fzf-share)/completion.$SHELL_NAME"
 fi
-
-command -v termux-reload-settings > /dev/null && ssh-picker
 
 export NODE_PATH=$HOME/.npm-global/lib/node_modules
