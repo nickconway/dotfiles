db4a14fcf98ccde6da4bf1807afe0a5424f5127e
Nick Conway <nick@conway.dev>
2023-11-09 23:04:07 -0500

    Update config

--- .config/shell/common/environment.sh
+++ .config/shell/common/environment.sh
@@ -38,7 +38,7 @@ fi
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 
-export HOMEBREW_PREFIX="$(brew --prefix)"
+command -v brew > /dev/null && export HOMEBREW_PREFIX="$(brew --prefix)"
 
 export VISUAL=nvim
 export EDITOR=nvim
