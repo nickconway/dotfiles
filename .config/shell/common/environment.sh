5c29b3469801509ed5529b917e9618641cb42671
Nick Conway <nick@conway.dev>
2024-01-01 16:59:51 -0500

    Update config

--- .config/shell/common/environment.sh
+++ .config/shell/common/environment.sh
@@ -55,6 +55,10 @@ export DROPBOX=/c/Users/nickc/Dropbox
 
 export TMUXP_CONFIGDIR=$HOME/.config/tmuxp/
 
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/.nvm")"
+[[ ! -e $NVM_DIR ]] && mkdir -p $NVM_DIR
+command -v brew > /dev/null && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use || [ -s "$NVM_DIR/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" --no-use # This loads nvm
+
 export FZF_DEFAULT_OPTS="--color=gutter:-1"
 
 # CTRL-/ to toggle small preview window to see the full command
