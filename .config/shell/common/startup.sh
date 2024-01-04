9b28d85c999590e8736325cbd1d87e88d296da5f
Nick Conway <nconway@fornetix.com>
2024-01-04 18:29:00 -0500

    Update config

--- .config/shell/common/startup.sh
+++ .config/shell/common/startup.sh
@@ -3,10 +3,9 @@ command -v starship > /dev/null && eval "$(starship init $SHELL_NAME)"
 command -v zoxide > /dev/null && eval "$(zoxide init $SHELL_NAME --cmd cd)"
 
 if [ -S $XDG_RUNTIME_DIR/agent.sock ]; then
+    export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/agent.sock
     if [[ "$(ssh-add -l)" == *"Error"* ]]; then
         rm $XDG_RUNTIME_DIR/agent.sock && eval $(ssh-agent -t 12h -s -a $XDG_RUNTIME_DIR/agent.sock)
-    else
-        export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/agent.sock
     fi
 else
     eval $(ssh-agent -t 12h -s -a $XDG_RUNTIME_DIR/agent.sock)
@@ -26,13 +25,13 @@ fi
 
 [[ -f ~/.fzf.$SHELL_NAME ]] && source ~/.fzf.$SHELL_NAME
 
-[[ "$(basename $SHELL)" == "$SHELL_NAME" ]] && export TMUX_SHELL="$SHELL" || export TMUX_SHELL="$SHELL_NAME"
-if [[ -z $TMUX ]] && [[ -z $ZELLIJ ]]; then
-    if [[ -z $SSH_CONNECTION ]]; then
-        command -v tmux > /dev/null && tmux new -As main $TMUX_SHELL && exit 0
-        # zellij a -c main
-    else
-        # zellij a -c ssh; exit
-        command -v tmux > /dev/null && tmux new -As ssh $TMUX_SHELL && exit 0
-    fi
-fi
+# [[ "$(basename $SHELL)" == "$SHELL_NAME" ]] && export TMUX_SHELL="$SHELL" || export TMUX_SHELL="$SHELL_NAME"
+# if [[ -z $TMUX ]] && [[ -z $ZELLIJ ]]; then
+#     if [[ -z $SSH_CONNECTION ]]; then
+#         command -v tmux > /dev/null && tmux new -As main $TMUX_SHELL && exit 0
+#         # zellij a -c main
+#     else
+#         # zellij a -c ssh; exit
+#         command -v tmux > /dev/null && tmux new -As ssh $TMUX_SHELL && exit 0
+#     fi
+# fi
