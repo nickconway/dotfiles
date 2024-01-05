46f84584da15fb72454ad19bb2c4b0f2a95c559c
Nick Conway <nick@conway.dev>
2024-01-05 18:42:45 -0500

    Update config

--- .config/shell/common/environment.sh
+++ .config/shell/common/environment.sh
@@ -28,6 +28,7 @@ fi
 KERNEL=$(uname -r)
 
 export PATH=~/.local/bin:~/bin:~/.npm-global/bin:~/.local/podman/bin:/home/linuxbrew/.linuxbrew/bin:~/.local/homebrew/bin:$HOME/.cargo/bin:~/.go/bin:$PATH
+export GOPATH=~/.go
 export GPG_TTY=$TTY
 
 if command -v termux-reload-settings > /dev/null; then
