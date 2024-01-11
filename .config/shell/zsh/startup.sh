ee899c92081f48d112040c807daa4813ee919b8a
Nick Conway <nick@conway.dev>
2024-01-11 15:15:44 -0500

    Update config

--- .config/shell/zsh/startup.sh
+++ .config/shell/zsh/startup.sh
@@ -1,3 +1,5 @@
+export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=~/.zsh/zsh-syntax-highlighting/highlighters
+
 [[ -e ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh ]] && source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 [[ -e ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]] && source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
