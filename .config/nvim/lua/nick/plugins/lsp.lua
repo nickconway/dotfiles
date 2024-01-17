68b0df1361ac9a89ae73f972ceb47ca53c8baa0f
Nick Conway <nick@conway.dev>
2024-01-17 16:49:03 -0500

    Update config

--- .config/nvim/lua/nick/plugins/lsp.lua
+++ .config/nvim/lua/nick/plugins/lsp.lua
@@ -1,6 +1,6 @@
 return {
     "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
+    branch = "v3.x",
     dependencies = {
         -- LSP Support
         { "neovim/nvim-lspconfig" },
@@ -16,6 +16,7 @@ return {
 
         -- Autocompletion
         { "hrsh7th/nvim-cmp" },
+        { "hrsh7th/cmp-cmdline" },
         { "hrsh7th/cmp-buffer" },
         { "hrsh7th/cmp-path" },
         { "saadparwaiz1/cmp_luasnip" },
@@ -33,14 +34,7 @@ return {
             library = { plugins = { "nvim-dap-ui" }, types = true },
         })
 
-        local lsp = require("lsp-zero").preset("recommended")
-
-        lsp.ensure_installed({
-            "tsserver",
-            "eslint",
-            "lua_ls",
-            "rust_analyzer",
-        })
+        local lsp = require("lsp-zero")
 
         lsp.on_attach(function(client, bufnr)
             if client.server_capabilities.documentSymbolProvider then
@@ -48,6 +42,22 @@ return {
             end
             lsp.default_keymaps({ buffer = bufnr })
         end)
+
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+            ensure_installed = {
+                "tsserver",
+                "eslint",
+                "lua_ls",
+                "rust_analyzer",
+            },
+            handlers = {
+                lsp.default_setup,
+                lua_ls = function()
+                    require('lspconfig').lua_ls.setup({})
+                end
+            },
+        })
 
         lsp.set_sign_icons({
             error = '✘',
@@ -62,17 +72,22 @@ return {
             end,
         })
 
-        lsp.nvim_workspace()
+        lsp.nvim_lua_ls()
         lsp.setup()
 
         local cmp = require('cmp')
         local cmp_action = require('lsp-zero').cmp_action()
 
         cmp.setup({
-            mapping = {
+            mapping = cmp.mapping.preset.insert({
                 ['<CR>'] = cmp.mapping.confirm({ select = false }),
                 ['<Tab>'] = cmp_action.luasnip_supertab(),
                 ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+            }),
+            sources = {
+                { name = 'nvim_lsp' },
+                { name = 'buffer' },
+                { name = 'cmp' },
             },
             formatting = {
                 fields = { 'abbr', 'kind', 'menu' },
@@ -83,6 +98,27 @@ return {
                     ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
                 })
             }
+        })
+
+        cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' }
+            }
+        })
+
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' }
+            }, {
+                {
+                    name = 'cmdline',
+                    option = {
+                        ignore_cmds = { 'Man', '!' }
+                    }
+                }
+            })
         })
     end,
 }
