434bb73290b997fd5689befe2320d1cbc9f84383
Nick Conway <nconway@fornetix.com>
2024-01-17 11:53:19 -0500

    Update config

--- .config/nvim/lua/nick/lazy.lua
+++ .config/nvim/lua/nick/lazy.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--single-branch",
-		"https://github.com/folke/lazy.nvim.git",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--single-branch",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+    })
 end
 vim.opt.runtimepath:prepend(lazypath)
 
@@ -15,7 +15,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 require("lazy").setup("nick.plugins", {
-	ui = {
-		border = "single",
-	},
+    ui = {
+        border = "single",
+    },
 })
