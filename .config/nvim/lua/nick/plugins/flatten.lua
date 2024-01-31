155e7c98c22faf4e9522c21eaf277d2ca7d75c88
Nick Conway <nick@conway.dev>
2024-01-31 12:50:05 -0500

    Update config

--- .config/nvim/lua/nick/plugins/flatten.lua
+++ .config/nvim/lua/nick/plugins/flatten.lua
@@ -1,12 +1,9 @@
-require("lazy").setup({
-    {
-        "willothy/flatten.nvim",
-        config = true,
-        -- or pass configuration with
-        -- opts = {  }
-        -- Ensure that it runs first to minimize delay when opening file from terminal
-        lazy = false,
-        priority = 1001,
-    },
-    --- ...
-})
+return {
+    "willothy/flatten.nvim",
+    config = true,
+    -- or pass configuration with
+    -- opts = {  }
+    -- Ensure that it runs first to minimize delay when opening file from terminal
+    lazy = false,
+    priority = 1001,
+}
