985fbb1944345d23508786e67634689f053caa79
Nick Conway <nick@conway.dev>
2024-02-12 11:25:15 -0500

    Update config

--- .config/shell/common/alias.sh
+++ .config/shell/common/alias.sh
@@ -222,6 +222,20 @@ alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
 alias tmuxconf='$EDITOR ~/.tmux.conf'
 
+function tu() {
+    if [[ $(uname -n) == "steamdeck" ]]; then
+        tailscale up --ssh --accept-routes --operator=$(whoami) --authkey "$VPN_KEY" --advertise-tags="tag:client,tag:server" --login-server https://vpn.conway.dev
+    elif [[ $(yadm config --get-all local.class) != *"server"* && $(yadm config --get-all local.class) != *"work"* ]]; then
+        tailscale up --ssh --accept-routes --operator=$(whoami) --authkey "$VPN_KEY" --advertise-tags="tag:client" --login-server https://vpn.conway.dev
+    else
+        if [[ $(yadm config --get-all local.class) != *"dns"* ]]; then
+            sudo tailscale up --ssh --accept-routes --authkey "$VPN_KEY" --advertise-tags="tag:server" --login-server https://vpn.conway.dev
+        else
+            sudo tailscale up --ssh --accept-routes --authkey "$VPN_KEY" --advertise-tags="tag:server" --accept-dns=false --login-server https://vpn.conway.dev
+        fi
+    fi
+}
+
 alias v="nvim"
 alias vm='s alma'
 
