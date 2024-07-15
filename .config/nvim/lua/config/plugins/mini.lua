e6feb6337a36f83f56bfdb7861e87991c62b4105
Nick <nick@conway.dev>
2024-07-14 22:58:35 -0400

    Whichkey nvim

--- .config/nvim/lua/config/plugins/mini.lua
+++ .config/nvim/lua/config/plugins/mini.lua
@@ -13,78 +13,78 @@ return {
         })
         require("mini.surround").setup()
 
-        local miniclue = require('mini.clue')
-        miniclue.setup({
-            triggers = {
-                -- Leader triggers
-                { mode = 'n', keys = '<Leader>' },
-                { mode = 'x', keys = '<Leader>' },
-
-                -- Built-in completion
-                { mode = 'i', keys = '<C-x>' },
-
-                -- `g` key
-                { mode = 'n', keys = 'g' },
-                { mode = 'x', keys = 'g' },
-
-                -- Marks
-                { mode = 'n', keys = "'" },
-                { mode = 'n', keys = '`' },
-                { mode = 'x', keys = "'" },
-                { mode = 'x', keys = '`' },
-
-                -- Registers
-                { mode = 'n', keys = '"' },
-                { mode = 'x', keys = '"' },
-                { mode = 'i', keys = '<C-r>' },
-                { mode = 'c', keys = '<C-r>' },
-
-                -- Window commands
-                { mode = 'n', keys = '<C-w>' },
-
-                -- `z` key
-                { mode = 'n', keys = 'z' },
-                { mode = 'x', keys = 'z' },
-            },
-
-            clues = {
-                -- Enhance this by adding descriptions for <Leader> mapping groups
-                miniclue.gen_clues.builtin_completion(),
-                miniclue.gen_clues.g(),
-                miniclue.gen_clues.marks(),
-                miniclue.gen_clues.registers({
-                    show_contents = true,
-                }),
-                miniclue.gen_clues.windows({
-                    submode_move = true,
-                    submode_navigate = true,
-                    submode_resize = true,
-                }),
-                miniclue.gen_clues.z(),
-
-                { mode = "n", keys = "<Leader>b", desc = "Buffers" },
-                { mode = "n", keys = "<Leader>d", desc = "DAP" },
-                { mode = "n", keys = "<Leader>f", desc = "Find" },
-                { mode = "n", keys = "<Leader>g", desc = "Git" },
-                { mode = "n", keys = "<Leader>h", desc = "Harpoon" },
-                { mode = "n", keys = "<Leader>l", desc = "Lazy" },
-                { mode = "n", keys = "<Leader>s", desc = "Save / Search" },
-                { mode = "n", keys = "<Leader>w", desc = "Window" },
-            },
-
-            window = {
-                -- Floating window config
-                config = {
-                    border = "rounded"
-                },
-
-                -- Delay before showing clue window
-                delay = 1000,
-
-                -- Keys to scroll inside the clue window
-                scroll_down = '<C-d>',
-                scroll_up = '<C-u>',
-            },
-        })
+        --     local miniclue = require('mini.clue')
+        --     miniclue.setup({
+        --         triggers = {
+        --             -- Leader triggers
+        --             { mode = 'n', keys = '<Leader>' },
+        --             { mode = 'x', keys = '<Leader>' },
+        --
+        --             -- Built-in completion
+        --             { mode = 'i', keys = '<C-x>' },
+        --
+        --             -- `g` key
+        --             { mode = 'n', keys = 'g' },
+        --             { mode = 'x', keys = 'g' },
+        --
+        --             -- Marks
+        --             { mode = 'n', keys = "'" },
+        --             { mode = 'n', keys = '`' },
+        --             { mode = 'x', keys = "'" },
+        --             { mode = 'x', keys = '`' },
+        --
+        --             -- Registers
+        --             { mode = 'n', keys = '"' },
+        --             { mode = 'x', keys = '"' },
+        --             { mode = 'i', keys = '<C-r>' },
+        --             { mode = 'c', keys = '<C-r>' },
+        --
+        --             -- Window commands
+        --             { mode = 'n', keys = '<C-w>' },
+        --
+        --             -- `z` key
+        --             { mode = 'n', keys = 'z' },
+        --             { mode = 'x', keys = 'z' },
+        --         },
+        --
+        --         clues = {
+        --             -- Enhance this by adding descriptions for <Leader> mapping groups
+        --             miniclue.gen_clues.builtin_completion(),
+        --             miniclue.gen_clues.g(),
+        --             miniclue.gen_clues.marks(),
+        --             miniclue.gen_clues.registers({
+        --                 show_contents = true,
+        --             }),
+        --             miniclue.gen_clues.windows({
+        --                 submode_move = true,
+        --                 submode_navigate = true,
+        --                 submode_resize = true,
+        --             }),
+        --             miniclue.gen_clues.z(),
+        --
+        --             { mode = "n", keys = "<Leader>b", desc = "Buffers" },
+        --             { mode = "n", keys = "<Leader>d", desc = "DAP" },
+        --             { mode = "n", keys = "<Leader>f", desc = "Find" },
+        --             { mode = "n", keys = "<Leader>g", desc = "Git" },
+        --             { mode = "n", keys = "<Leader>h", desc = "Harpoon" },
+        --             { mode = "n", keys = "<Leader>l", desc = "Lazy" },
+        --             { mode = "n", keys = "<Leader>s", desc = "Save / Search" },
+        --             { mode = "n", keys = "<Leader>w", desc = "Window" },
+        --         },
+        --
+        --         window = {
+        --             -- Floating window config
+        --             config = {
+        --                 border = "rounded"
+        --             },
+        --
+        --             -- Delay before showing clue window
+        --             delay = 1000,
+        --
+        --             -- Keys to scroll inside the clue window
+        --             scroll_down = '<C-d>',
+        --             scroll_up = '<C-u>',
+        --         },
+        --     })
     end,
 }
---
+++ .config/nvim/lua/config/plugins/which-key.lua
@@ -0,0 +1,30 @@
+return {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    },
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
+    config = function()
+        require("which-key").add({
+            { "<leader>b", group = "Buffers" },
+            { "<leader>d", group = "DAP" },
+            { "<leader>f", group = "Find" },
+            { "<leader>g", group = "Git" },
+            { "<leader>h", group = "Harpoon" },
+            { "<leader>l", group = "Lazy" },
+            { "<leader>s", group = "Save / Search" },
+            { "<leader>w", group = "Window" },
+        })
+    end
+}
