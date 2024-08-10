f0902b191197982e9dde64ece0b983d6569d9787
Nick <nick@conway.dev>
2024-08-10 18:26:18 -0400

    persistence condition

--- .config/nvim/lua/config/plugins/persistence.lua
+++ .config/nvim/lua/config/plugins/persistence.lua
@@ -1,5 +1,6 @@
 return {
     "folke/persistence.nvim",
     event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    enabled = os.getenv("AUTOSESSION_ENABLED") ~= "no",
     opts = {},
 }
