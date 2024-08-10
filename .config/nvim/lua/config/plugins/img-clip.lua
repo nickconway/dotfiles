d1ec681db97bed458c693b86f043b290d8913053
Nick <nick@conway.dev>
2024-08-10 16:31:23 -0400

    Change paste image keybind

--- .config/nvim/lua/config/plugins/img-clip.lua
+++ .config/nvim/lua/config/plugins/img-clip.lua
@@ -1,12 +1,8 @@
 return {
     "HakonHarnes/img-clip.nvim",
     event = "VeryLazy",
-    opts = {
-        -- add options here
-        -- or leave it empty to use the default settings
-    },
+    opts = {},
     keys = {
-        -- suggested keymap
-        { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+        { "<leader>P", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
     },
 }
