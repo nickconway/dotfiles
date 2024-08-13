55b66a7d12dfe34dda29085ed99251fadbdc51b9
Nick <nick@conway.dev>
2024-08-12 22:24:58 -0400

    alpha

--- .config/nvim/lua/config/plugins/alpha.lua
+++ .config/nvim/lua/config/plugins/alpha.lua
@@ -1,6 +1,50 @@
 return {
     "goolord/alpha-nvim",
     opts = function()
+        local function is_git_repo()
+            vim.fn.system("git rev-parse --is-inside-work-tree")
+            return vim.v.shell_error == 0
+        end
+
+        local function get_git_root()
+            local dot_git_path = vim.fn.finddir(".git", ".;")
+            return vim.fn.fnamemodify(dot_git_path, ":h")
+        end
+
+        local builtin = require("telescope.builtin")
+        local pickers = require("telescope.pickers")
+        local finders = require("telescope.finders")
+        local conf = require("telescope.config").values
+        local function get_files()
+            if vim.fn.getcwd() == vim.fn.getenv("HOME") then
+                local topts = {}
+
+                topts.entry_maker = function(entry)
+                    local str = entry
+                    str = string.gsub(str, ".config/yadm/alt/", "")
+                    str = string.gsub(str, "##template.*", "")
+                    return {
+                        value = string.format("%s/%s", vim.fn.getenv("HOME"), entry),
+                        display = str,
+                        ordinal = entry,
+                    }
+                end
+
+                pickers
+                    .new(topts, {
+                        prompt_title = "Yadm Files",
+                        finder = finders.new_oneshot_job({ "get-yadm-files" }, topts),
+                        previewer = conf.file_previewer(topts),
+                        sorter = conf.file_sorter(topts),
+                    })
+                    :find()
+            elseif is_git_repo() then
+                builtin.git_files({ cwd = get_git_root() })
+            else
+                builtin.find_files()
+            end
+        end
+
         local dashboard = require("alpha.themes.dashboard")
         dashboard.section.header.val = {
             [[⠭⠵⠖⠀⠀⠀⠀⠀⠀⠀⠠⠀⠠⠀⡠⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠄⠠⠀⠀⠀  ]],
@@ -42,7 +86,7 @@ return {
 
         dashboard.section.buttons.val = {
             dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-            dashboard.button("<leader>ff", "  Find file", ":Telescope find_files hidden=true no_ignore=true<CR>"),
+            dashboard.button("<leader>ff", "  Find file", get_files),
             dashboard.button("s", "󰋃  Open last session", function()
                 require("persistence").load()
             end),
