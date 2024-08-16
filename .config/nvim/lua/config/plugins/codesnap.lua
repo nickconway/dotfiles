265ade25384bca2b675d1c9bd1954a3479256af8
Nick <nick@conway.dev>
2024-08-15 20:03:57 -0400

    codesnap

---
+++ .config/nvim/lua/config/plugins/codesnap.lua
@@ -0,0 +1,30 @@
+return {
+    "mistricky/codesnap.nvim",
+    build = "make build_generator",
+    keys = {
+        {
+            "<leader>cc",
+            "<esc><cmd>lua vim.fn.mkdir(\"" ..
+            os.getenv("HOME") .. "/Pictures/CodeSnap\", \"p\")<cr>" .. "<cmd>CodeSnapSave<cr>",
+            mode = "x",
+            desc = "Save selected code snapshot into clipboard"
+        },
+        {
+            "<leader>cC",
+            "<esc><cmd>lua vim.fn.mkdir(\"" ..
+            os.getenv("HOME") .. "/Pictures/CodeSnap\", \"p\")<cr>" .. "<cmd>CodeSnapSaveHighlight<cr>",
+            mode = "x",
+            desc = "Save selected code snapshot into clipboard"
+        },
+    },
+    opts = {
+        save_path = "~/Pictures/CodeSnap",
+        code_font_family = "Hack Nerd Font",
+        has_breadcrumbs = true,
+        bg_theme = "summer",
+        watermark = "",
+        mac_window_bar = false,
+        bg_x_padding = 50,
+        bg_y_padding = 40,
+    },
+}
