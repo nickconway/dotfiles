8fd2cf058c585cdd06bcde1839c599b07bcb4286
Nick Conway <nick@conway.dev>
2024-09-03 15:38:02 -0400

    restlast

--- .config/nvim/lua/config/plugins/rest.lua
+++ .config/nvim/lua/config/plugins/rest.lua
@@ -1,7 +1,8 @@
 return {
     "rest-nvim/rest.nvim",
     keys = {
-        { "<leader>rr", "<cmd>Rest run<cr>", desc = "Rest Run Under Cursor" }
+        { "<leader>rr", "<cmd>Rest run<cr>", desc = "Rest Run Under Cursor" },
+        { "<leader>rl", "<cmd>Rest last<cr>", desc = "Rest Run Last" }
     },
     config = function()
         ---@type rest.Opts
