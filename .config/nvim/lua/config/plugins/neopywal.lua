b15f001c075ab10814ada6d5d988bb8f8a0f1ca9
Nick Conway <nick@conway.dev>
2024-10-04 19:59:42 -0400

    fix neopywal when pywal not installed

--- .config/nvim/lua/config/plugins/neopywal.lua
+++ .config/nvim/lua/config/plugins/neopywal.lua
@@ -1,7 +1,13 @@
+local function file_exists(name)
+    local f = io.open(name, "r")
+    return f ~= nil and io.close(f)
+end
+
 return {
     "RedsXDD/neopywal.nvim",
     name = "neopywal",
     lazy = false,
+    enabled = file_exists(os.getenv("HOME") .. "/.cache/wal/colors"),
     priority = 1000,
     opts = {
         transparent_background = true,
