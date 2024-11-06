f33a7d06afae5c43d1e1d85c26445077a2b69798
Nick Conway <nick@conway.dev>
2024-11-06 16:41:33 -0500

    fix gitsigns yadm url

--- .config/nvim/lua/config/plugins/gitsigns.lua
+++ .config/nvim/lua/config/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ return {
     'lewis6991/gitsigns.nvim',
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "seanbreckenridge/gitsigns-yadm.nvim",
+        "purarue/gitsigns-yadm.nvim",
     },
     event = "BufEnter",
     keys = {
