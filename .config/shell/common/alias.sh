5d5bc6836900c87d1a3568334a3280d59b795c04
Nick Conway <nick@conway.dev>
2025-01-26 14:17:07 -0500

    talos

--- .config/shell/common/alias.sh
+++ .config/shell/common/alias.sh
@@ -667,6 +667,10 @@ function tu() {
     eval "$TS_COMMAND set --auto-update"
 }
 
+function talos-batch() {
+    kubectl get nodes -o wide | awk '{print $6}' | tail -n +2 | xargs | sed 's/ /,/g' | xargs talosctl "$1" -n
+}
+
 alias v='$EDITOR'
 function venv() {
     if [[ ! -e .venv ]]; then
