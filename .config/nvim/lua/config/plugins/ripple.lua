61d40bdda751a9c649950278adf4d81e6fa8a57b
Nick Conway <nick@conway.dev>
2025-02-22 12:27:11 -0500

    add ripple.nvim

---
+++ .config/nvim/lua/config/plugins/ripple.lua
@@ -0,0 +1,17 @@
+return {
+    "ian-howell/ripple.nvim",
+    keys = {
+        "<M-C-Up>",
+        "<M-C-Down>",
+        "<M-C-Left>",
+        "<M-C-Right>",
+    },
+    opts = {
+        keys = {
+            expand_up = "<M-C-Up>",
+            expand_down = "<M-C-Down>",
+            expand_right = "<M-C-Right>",
+            expand_left = "<M-C-Left>",
+        },
+    },
+}
