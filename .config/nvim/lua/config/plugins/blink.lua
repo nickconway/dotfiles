017015266cc9a9cf9070ec2127f86df0a965ec01
Nick Conway <nick@conway.dev>
2025-03-29 09:41:08 -0400

    fix esc hitting enter in command mode with blink

--- .config/nvim/lua/config/plugins/blink.lua
+++ .config/nvim/lua/config/plugins/blink.lua
@@ -73,7 +73,16 @@ return {
                 preset = 'none',
                 ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
                 ['<CR>'] = { 'accept', 'fallback' },
-                ['<Esc>'] = { 'hide', 'fallback' },
+                ['<Esc>'] = {
+                    'hide',
+                    function(cmp)
+                        if cmp.is_visible() then
+                            cmp.cancel()
+                        else
+                            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-c>", true, true, true), "n", true)
+                        end
+                    end
+                },
 
                 ['<Tab>'] = {
                     function(cmp)
