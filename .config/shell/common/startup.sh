6bccca6f084a5f3e95686f1b952f4238f46dae5d
Nick Conway <nick@conway.dev>
2025-05-10 13:32:02 -0400

    update shell startup for auto tmux session names

--- .config/shell/common/startup.sh
+++ .config/shell/common/startup.sh
@@ -34,14 +34,19 @@ if command -v steamos-update > /dev/null; then
     fi
 else
     if [[ -z "$NO_TMUX" ]]; then
-        [[ "$(basename $SHELL)" == "$SHELL_NAME" ]] && export TMUX_SHELL="$SHELL" || export TMUX_SHELL="$SHELL_NAME"
-        if [[ -n "$PS1" ]] && [[ -z $TMUX ]] && [[ -z $ZELLIJ ]]; then
-            if [[ -z $SSH_CONNECTION ]]; then
-                command -v tmux > /dev/null && tmux -u new -As Main $TMUX_SHELL &> /dev/null && exit 0
+        [[ "$(basename "$SHELL")" == "$SHELL_NAME" ]] && export TMUX_SHELL="$SHELL" || export TMUX_SHELL="$SHELL_NAME"
+        if [[ -n "$PS1" ]] && [[ -z "$TMUX" ]] && [[ -z "$ZELLIJ" ]]; then
+            if [[ -z "$SSH_CONNECTION" ]]; then
+                if [[ -n "$TMUX_AUTO_SESSION" ]]; then
+                    echo "$TMUX_AUTO_SESSION"
+                    command -v tmux > /dev/null && tmux -u new "$TMUX_SHELL" &> /dev/null && exit 0
+                else
+                    command -v tmux > /dev/null && tmux -u new -As "${TMUX_SESSION_NAME:-Main}" "$TMUX_SHELL" &> /dev/null && exit 0
+                fi
                 # zellij a -c main
             else
                 # zellij a -c ssh; exit
-                command -v tmux > /dev/null && tmux -u new -As SSH $TMUX_SHELL &> /dev/null && exit 0
+                command -v tmux > /dev/null && tmux -u new -As SSH "$TMUX_SHELL" &> /dev/null && exit 0
             fi
         fi
     fi
