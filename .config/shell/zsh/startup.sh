d125db1487d5c234425fcd0cd1fb042026c6a2bb
Nick Conway <nick@conway.dev>
2025-08-21 10:56:09 -0400

    update zsh sourcing

--- .config/shell/zsh/startup.sh
+++ .config/shell/zsh/startup.sh
@@ -1,5 +1,15 @@
-[[ -e ~/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh ]] && source ~/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
-[[ -e ~/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]] && source ~/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [[ -e ~/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
+    source ~/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+elif [[ -e $HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]]; then
+    source $HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
+
+if [[ -e ~/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+    source ~/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+elif [[ -e $HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+    source $HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
+
 [[ -e ~/.config/zsh/fzf-tab/fzf-tab.plugin.zsh ]] && source ~/.config/zsh/fzf-tab/fzf-tab.plugin.zsh
 [[ -e ~/.config/zsh/sudo.plugin.zsh ]] && source ~/.config/zsh/sudo.plugin.zsh
 [[ -e ~/.config/zsh/command-not-found.plugin.zsh ]] && source ~/.config/zsh/command-not-found.plugin.zsh
