867e9debc14002097502d3076e87200021480055
Nick Conway <nick@conway.dev>
2025-11-20 12:40:01 -0500

    Update nvim leneline config

--- .config/nvim/lua/plugins/lensline.lua
+++ .config/nvim/lua/plugins/lensline.lua
@@ -1,8 +1,25 @@
 return {
     'oribarilan/lensline.nvim',
-    tag = '1.0.0', -- or: branch = 'release/1.x' for latest non-breaking updates
+    branch = 'release/2.x',
     event = 'LspAttach',
-    config = function()
-        require("lensline").setup()
-    end,
+    opts = {
+        profiles = {
+            {
+                name = 'minimal',
+                style = {
+                    placement = 'inline',
+                    prefix = '',
+                    -- render = "focused", optionally render lenses only for focused function
+                }
+            }
+        }
+    },
+    keys = {
+        {
+            "<leader>uo",
+            "<cmd>LenslineToggleView<cr>",
+            mode = "n",
+            desc = "Toggle Lensline"
+        }
+    }
 }
