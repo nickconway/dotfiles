b7156058d3c389b349dbd8e97954a558ca717258
Nick Conway <nick@conway.dev>
2025-11-21 22:25:32 -0500

    Remove mason-confirm.nvim

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,12 +1,9 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "black" },
-			},
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "black" },
 		},
 	},
-	{ "LittleEndianRoot/mason-conform" },
 }
