252eb26a22560c3d9c44b40a3f5b45dda4501797
Nick Conway <nick@conway.dev>
2026-05-16 11:30:23 -0400

    Update hyprland config

--- .config/hypr/include/monitors.lua
+++ .config/hypr/include/monitors.lua
@@ -5,5 +5,6 @@ hl.monitor({ output = "DP-1", mode = "preferred", scale = "1.33", bitdepth = 10 
 hl.monitor({ output = "DP-2", mode = "preferred", scale = "1.33", bitdepth = 10, position = "auto-left" })
 hl.monitor({ output = "DP-3", mode = "preferred", scale = "1.33", bitdepth = 10, position = "auto-right" })
 
--- hl.monitor({ output = "eDP-2", position = "auto-down", scale = "1.6", bitdepth = 10 })
+hl.monitor({ output = "eDP-1", mode = "preferred", position = "auto", scale = "1.6", bitdepth = 10 })
+-- hl.monitor({ output = "eDP-2", mode = "preferred", position = "auto-down", scale = "1.6", bitdepth = 10 })
 hl.monitor({ output = "eDP-2", disabled = true })
