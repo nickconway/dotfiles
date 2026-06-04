7c1e668804a64798faf877371440093cb5557436
Nick Conway <nick@conway.dev>
2026-06-04 09:54:28 -0400

    Update hyprland config

--- .config/hypr/include/workspaces.lua
+++ .config/hypr/include/workspaces.lua
@@ -19,8 +19,8 @@ if Run("hostnamectl hostname") == "desktop" then
     Bind({ MainMod, "F" }, hl.dsp.focus({ window = "feishin" }))
 
     hl.workspace_rule({ workspace = "1", monitor = "DP-1", default = true })
-    hl.workspace_rule({ workspace = "2", monitor = "DP-2", default = true })
-    hl.workspace_rule({ workspace = "3", monitor = "DP-3", default = true })
+    hl.workspace_rule({ workspace = "2", monitor = "DP-3", default = true })
+    hl.workspace_rule({ workspace = "3", monitor = "DP-2", default = true })
 
     hl.on("hyprland.start", function()
         hl.exec_cmd("[workspace 1 silent] chromium")
