c12ab52d2679bae4400d287bcc28cd21bb9e4f23
Nick Conway <nick@conway.dev>
2026-06-07 02:05:32 -0400

    openrgb

--- .config/hypr/include/startup.lua
+++ .config/hypr/include/startup.lua
@@ -29,6 +29,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("zenbook-duo-auto-brightness")
 
     hl.exec_cmd("bitwarden")
+    hl.exec_cmd("openrgb")
 
     hl.exec_cmd("LOG=y ups-commands")
 
