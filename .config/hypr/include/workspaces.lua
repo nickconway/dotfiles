22e8448ae677b4160edd2eea494be8bfc4eb9888
Nick Conway <nick@conway.dev>
2026-07-02 12:39:56 -0400

    update hyprland config

--- .config/hypr/include/workspaces.lua
+++ .config/hypr/include/workspaces.lua
@@ -41,7 +41,22 @@ hl.workspace_rule({ workspace = "name:game", monitor = "1", default = false })
 
 hl.on("window.destroy", function()
     if hl.get_active_workspace().name == "game" and (hl.get_active_workspace().windows == 0) then
-        hl.dispatch(hl.dsp.focus({ workspace = hl.get_last_workspace(hl.get_active_monitor()) }))
+        local ws = hl.get_last_workspace(hl.get_active_monitor())
+
+        if ws == nil then
+            for _, w in ipairs(hl.get_workspaces()) do
+                if w.monitor.id == hl.get_active_monitor().id then
+                    ws = w
+                    break
+                end
+            end
+        end
+
+        if ws == nil then
+            return
+        end
+
+        hl.dispatch(hl.dsp.focus({ workspace = ws }))
     end
 end)
 
