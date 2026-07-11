0702cd09811c6a688c928f05c1c4dc8faae0c652
Nick Conway <nick@conway.dev>
2026-07-11 15:38:15 -0400

    update startup

--- .config/shell/common/_startup.sh
+++ .config/shell/common/_startup.sh
@@ -7,7 +7,7 @@ export FZF_DEFAULT_OPTS='--tmux --reverse --min-height 20 --cycle -m --pointer=â
     '
 
 if command -v steamos-update &>/dev/null; then
-    distrobox enter arch --additional-flags "--env DISPLAY=${DISPLAY}" && exit
+    distrobox enter arch --additional-flags "--env DISPLAY=${DISPLAY} --env SSH_CONNECTION=$SSH_CONNECTION" && exit
 fi
 
 if [[ -z "$NO_TMUX" ]]; then
