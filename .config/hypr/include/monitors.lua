ea7e644689b5c669220df94d1a248ee7d28b749f
Nick Conway <nick@conway.dev>
2026-07-11 22:28:09 -0400

    update sunshine scripts

--- .config/hypr/include/monitors.lua
+++ .config/hypr/include/monitors.lua
@@ -1,2 +1,4 @@
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "1" })
-hl.monitor({ output = "Sunshine", disabled = true })
+hl.monitor({ output = "Sunshine", position = "100000x100000" })
+
+hl.exec_cmd("hyprctl output create headless Sunshine")
