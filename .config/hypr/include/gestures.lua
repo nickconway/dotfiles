4211d78182b758f00fcc62646dec1b383e42a9ad
Nick Conway <nick@conway.dev>
2026-07-29 11:16:25 -0400

    update hyprland config

--- .config/hypr/include/gestures.lua
+++ .config/hypr/include/gestures.lua
@@ -1,3 +1,9 @@
+hl.gesture({
+    fingers = 3,
+    direction = "horizontal",
+    action = hl.get_active_workspace().tiled_layout == "scrolling" and "scroll_move" or "workspace",
+})
+
 hl.on("workspace.active", function()
     hl.gesture({
         fingers = 3,
@@ -8,7 +14,7 @@ end)
 
 if hl.plugin.scrolloverview then
     hl.gesture({
-        fingers = 4,
+        fingers = 3,
         direction = "up",
         action = function()
             hl.plugin.scrolloverview.overview("toggle")
