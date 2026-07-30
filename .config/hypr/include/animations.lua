589925c1f5ad4b1da56e8d1a12ea4c848ea8dfd6
Nick Conway <nick@conway.dev>
2026-07-30 14:58:21 -0400

    update hyprlock

--- .config/hypr/include/animations.lua
+++ .config/hypr/include/animations.lua
@@ -1,8 +1,8 @@
-hl.curve("Bouncy", { type = "bezier", points = { { 0, 0.9 }, { 0.05, 1.5 } } })
 hl.curve("Linear", { type = "bezier", points = { { 0, 0 }, { 1, 1 } } })
+hl.curve("Bouncy", { type = "bezier", points = { { 0, 0.9 }, { 0.05, 1.5 } } })
 
 hl.animation({ leaf = "windows", enabled = true, speed = 2, bezier = "Bouncy" })
-hl.animation({ leaf = "windowsIn", enabled = true, speed = 4, bezier = "Bouncy", style = "popin 80%" })
+hl.animation({ leaf = "windowsIn", enabled = true, speed = 4, bezier = "Bouncy", style = "popin" })
 hl.animation({ leaf = "windowsOut", enabled = true, speed = 4, bezier = "Bouncy", style = "popin 80%" })
 hl.animation({ leaf = "border", enabled = true, speed = 15, bezier = "Linear" })
 hl.animation({ leaf = "fade", enabled = true, speed = 5, bezier = "Linear" })
