f2ec01c1d9735f57fe8a54715ea088b50870dac4
Nick Conway <nick@conway.dev>
2026-08-01 18:10:07 -0400

    update scripts

--- .config/hypr/include/windows.lua
+++ .config/hypr/include/windows.lua
@@ -226,7 +226,7 @@ hl.on("window.title", function(w)
 end)
 
 hl.on("window.open", function(w)
-    if hl.get_active_special_workspace() then
+    if not w.floating and hl.get_active_special_workspace() then
         hl.dispatch(hl.dsp.window.move({ workspace = hl.get_active_workspace(), window = w }))
         hl.dispatch(hl.dsp.focus({ window = w }))
     end
